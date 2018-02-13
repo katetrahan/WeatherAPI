9bb1b28eda5f7c4060ce940f8749b980413113b5
Kate Trahan and Ross Fletcher <kate@>
2018-02-13 09:33:30 -0800

    add weather api

---
+++ js/weather-interface.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0861176a572e9a20ec6bb0e19215126c`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.")
+      }
+    });
+  });
+});
